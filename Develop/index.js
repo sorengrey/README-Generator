943c53dcf5248cb2c86652e9c387d94302f388e8
Heather Smith <halexsmith86@gmail.com>
2021-02-15 16:58:19 -0500

    Filled out questions array

--- Develop/index.js
+++ Develop/index.js
@@ -1,7 +1,12 @@
 // TODO: Include packages needed for this application
+const fs = require('fs');
+const util = require('util');
+const inquirer = require('inquirer');
+// this probably isn't right
+const generateMarkdown = require('package');
 
-// TODO: Create an array of questions for user input
-const questions = [];
+// An array of questions for user input - done
+const questions = ['What is the title of your project?', 'Please enter a description for your project.', 'How is your project installed?', 'What usage information would you like to provide?', 'What are your contribution guidelines?', 'What are your test instructions?', 'Which license is your project protected under?', 'What is your GitHub username?', 'What is your email address?'];
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {}
