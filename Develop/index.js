7e4befea000ad8b7de933ab4aeaaa4492353cd45
Heather Smith <halexsmith86@gmail.com>
2021-02-17 13:38:24 -0500

    Added writeToFile function

--- Develop/index.js
+++ Develop/index.js
@@ -2,17 +2,24 @@
 const fs = require('fs');
 const util = require('util');
 const inquirer = require('inquirer');
-// this probably isn't right
-const generateMarkdown = require('package');
 
-// An array of questions for user input - done
+console.log(process.argv);
+
+// This doesn't work yet -- gives a module_not_found error
+//const generateMarkdown = require('/generateMarkdown.js');
+
+// An array of questions for user input - will probably need another array for the license options for the use to select from
 const questions = ['What is the title of your project?', 'Please enter a description for your project.', 'How is your project installed?', 'What usage information would you like to provide?', 'What are your contribution guidelines?', 'What are your test instructions?', 'Which license is your project protected under?', 'What is your GitHub username?', 'What is your email address?'];
 
-// TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+// TODO: Create a function to write README file - figure out how to put your data in
+function writeToFile(fileName, data) {
+    fs.writeFile('new-readme.md', data, function (err) {
+        if (err) return console.log(err);
+    })
+}
 
 // TODO: Create a function to initialize app
-function init() {}
+function init() { }
 
 // Function call to initialize app
 init();
