36787bf0da56d2942390975f71ae3f2ce73f829e
Heather Smith <halexsmith86@gmail.com>
2021-02-17 16:15:04 -0500

    Added inquirer to questions array
    Program now prompts the user with questions

--- Develop/index.js
+++ Develop/index.js
@@ -3,14 +3,64 @@ const fs = require('fs');
 const util = require('util');
 const inquirer = require('inquirer');
 const generateMarkdown = require("./generateMarkdown.js");
+// you might need one for the badges -- check npmjs.com's libraries
 
 console.log(process.argv);
 // console.log(fs);
 
-// An array of questions for user input - will probably need another array for the license options for the use to select from
-const questions = ['What is the title of your project?', 'Please enter a description for your project.', 'How is your project installed?', 'What usage information would you like to provide?', 'What are your contribution guidelines?', 'What are your test instructions?', 'Which license is your project protected under?', 'What is your GitHub username?', 'What is your email address?'];
+// An array of questions for user input
+inquirer
+    .prompt([
+        {
+            type: 'input',
+            message: 'What is the title of your project?',
+            name: 'projectname',
+        },
+        {
+            type: 'input',
+            message: 'Please enter a description for your project.',
+            name: 'description'
+        },
+        {
+            type: 'input',
+            message: 'How is your project installed?',
+            name: 'installation',
+        },
+        {
+            type: 'input',
+            message: 'What usage information would you like to provide?',
+            name: 'usage',
+        },
+        {
+            type: 'input',
+            message: 'What are your contribution guidelines?',
+            name: 'contributions',
+        },
+        {
+            type: 'input',
+            message: 'What are your test instructions?',
+            name: 'test',
+        },
+        {
+            type: 'input',
+            message: 'Which license is your project protected under?',
+            name: 'license',
+        },
+        {
+            type: 'input',
+            message: 'What is your GitHub username?',
+            name: 'github',
+        },
+        {
+            type: 'input',
+            message: 'What is your email address?',
+            name: 'email',
+        },
+    ])
+    .then((response) => {
+});
 
-// TODO: Create a function to write README file - figure out how to put your data in
+// TODO: Create a function to write README file - doesn't work yet - figure out how to put response in there
 function writeToFile(fileName, data) {
     fs.appendFile('new-readme.md', data, (err) => {
         err ? console.error(err) : console.log('success');
